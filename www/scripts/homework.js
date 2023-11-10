dd96e57082a2c8769ceda280d35bab2ce8c0586a
dredgenkell <lorecryptarchy@gmail.com>
2023-11-10 22:05:50 +0300

    Part 1 Lessons progression

---
+++ www/scripts/homework.js
@@ -0,0 +1,23 @@
+// let country = 'Norway';
+// console.log(country);
+
+// let continent = 'Eurasia';
+// console.log(continent);
+
+let population = 5.408;
+
+// let isIsland = false;
+
+let language;
+
+
+language = 'Norse';
+const continent = 'Eurasia';
+const isIsland = false;
+const country = 'Norway'
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,3 +1,6 @@
+/*
+
+DATA TYPES
 
 let js = 'amazing';
 console.log(22 + 227);
@@ -30,3 +33,47 @@ console.log(continent)
 
 let population = '5.408 million'
 console.log(population)
+
+
+
+// VALUES AND TYPES
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun)
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Nami');
+
+javaScriptIsFun = 'Yes?'
+
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year)
+
+year = 1994;
+console.log(year)
+
+console.log(typeof null);
+
+
+// LET,CONST,VAR
+
+let age = 30;
+age = 29;
+
+const birthYear = 1994;
+// birthYear = 1998;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Yamato'
+
+console.log(lastName);
+*/
