78d55ee77ce6157f9f98db896ed5b4c27d3e31dc
dredgenkell <lorecryptarchy@gmail.com>
2023-11-10 20:52:23 +0300

    preparations fo js course

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,32 @@
-console.log('script work!');
+
+let js = 'amazing';
+console.log(22 + 227);
+
+console.log("Jonas")
+console.log(29)
+
+let firstName = 'Nami'
+
+
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+// let jonas$Nami = "JN";
+// let $function = 27;
+
+// let Person = 'Nami';
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Engineer';
+
+console.log(myCurrentJob)
+
+let country = 'Norway';
+console.log(country);
+
+let continent = 'Eurasia';
+console.log(continent)
+
+let population = '5.408 million'
+console.log(population)
